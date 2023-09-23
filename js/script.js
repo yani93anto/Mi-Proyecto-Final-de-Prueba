42b3126165575a9729a3f3461613e863d80fd3a0
yani93anto <cammarasana.yanina@gmail.com>
2023-09-23 18:25:35 -0300

    carrusel

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const productsContainer = document.getElementById("products");
-
-    // Cargar productos desde JSON usando Fetch API
+document.addEventListener("DOMContentLoaded", function() {
+    // Realiza una solicitud Fetch para cargar datos desde el archivo JSON
     fetch("json/products.json")
-        .then(response => response.json())
-        .then(products => {
-            products.forEach(product => {
-                const productCard = createProductCard(product);
-                productsContainer.appendChild(productCard);
-            });
-        })
-        .catch(error => console.error("Error al cargar productos: " + error));
-
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("No se pudo cargar el archivo JSON.");
+        }
+        return response.json();
+      })
+      .then(products => {
+      const productsContainer = document.getElementById("products");
+  
+      products.forEach(producto => {
+        const productCard = createProductCard(producto);
+        const card = document.createElement("div");
+        card.classList.add("product-card");
+  
+        const carousel = document.createElement("div");
+        carousel.classList.add("product-images");
+  
+        producto.image.forEach(imagen => {
+          const img = document.createElement("img");
+          img.src = imagen;
+          carousel.appendChild(img);
+        });
+  
+        productCard.appendChild(carousel);
+        productsContainer.appendChild(productCard);
+      });
+  
+      // Inicializa Slick Carousel
+      $('.product-images').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000 // Velocidad de reproducción automática en milisegundos
+    });
+})
+.catch(error => {
+  console.error(error);
+})
     // Función para crear un elemento de producto en el DOM
     function createProductCard(product) {
         const productCard = document.createElement("div");
