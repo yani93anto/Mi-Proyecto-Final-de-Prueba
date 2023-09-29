f48441e60ea3357f8f594c5a4e5edd922023c1b7
yani93anto <cammarasana.yanina@gmail.com>
2023-09-29 20:18:21 -0300

    contactos

--- js/script _producto.js
+++ js/script _producto.js
@@ -40,3 +40,30 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
   
+
+/* Dark Mode */
+// Obtén una referencia al botón "Modo Oscuro"
+var boton = document.getElementById('boton');
+
+// Obtén una referencia al cuerpo de la página
+var body = document.body;
+
+// Agrega un listener al botón para cambiar el modo
+boton.addEventListener('click', function() {
+    body.classList.toggle('dark-mode');// Muestra una confirmación
+})
+
+function cambioDeModo() {
+    const boton = document.getElementById('boton');
+    const body = document.body;
+  
+    if (body.classList.contains('modo-oscuro')) {
+      // Cambia a modo claro
+      body.classList.remove('modo-oscuro');
+      boton.innerText = 'Modo Oscuro';
+    } else {
+      // Cambia a modo oscuro
+      body.classList.add('modo-oscuro');
+      boton.innerText = 'Modo Claro';
+    }
+  }
